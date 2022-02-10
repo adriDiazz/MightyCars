86dc0c4353f1097af3078a88ddd5a9d849fa42bc
Jonathan Urrutia <urrutiab.jonathan@gmail.com>
2022-02-09 21:06:02 -0300

    v0_back

---
+++ mightyCars/src/main/java/com/mightycars/MightyCarsApplication.java
@@ -0,0 +1,13 @@
+package com.mightycars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MightyCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MightyCarsApplication.class, args);
+	}
+
+}
