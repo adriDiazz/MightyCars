2bd90858fea358be7c2450ffae0edaec4e386f72
Jonathan Urrutia <urrutiab.jonathan@gmail.com>
2022-02-15 23:49:17 -0300

    Solución CORS

--- mightyCars/src/main/java/com/mightycars/MightyCarsApplication.java
+++ mightyCars/src/main/java/com/mightycars/MightyCarsApplication.java
@@ -2,12 +2,25 @@ package com.mightycars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MightyCarsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MightyCarsApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://mightycars.herokuapp.com/").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
