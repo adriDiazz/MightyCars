334283013c6e067082a140e953ef510f02bbc27b
Javier B <javierboscan16@gmail.com>
2022-02-22 16:02:18 -0400

    eliminando codigo comentado, creando cart.css y formateando codigo con prettier

--- js/cart.js
+++ js/cart.js
@@ -2,31 +2,27 @@ const carrContainer = document.querySelector('.cards__cart');
 const btnBuy = document.querySelector('.btn-buy');
 console.log(btnBuy);
 
-cartTimes = localStorage.getItem('cartTimes')
+cartTimes = localStorage.getItem('cartTimes');
 
 carsOnCart = [];
 
-
-
-for (var i = 0; i < cartTimes; i++){
-    carsOnCart.push(localStorage.getItem(`cart${i+1}`))
+for (let i = 0; i < cartTimes; i++) {
+	carsOnCart.push(localStorage.getItem(`cart${i + 1}`));
 }
 
-console.log(carsOnCart)
-carsOnCart.map(car => {
-    const tpl = `<div class="card__cart_product">
+console.log(carsOnCart);
+carsOnCart.map((car) => {
+	const tpl = `<div class="card__cart_product">
     <img src="./img/Camioneta_FFFFF_1.png" alt="">
     <div class="info__card_cart">
       <span class="span__name">${car}</span>
       <span>Entrega estimada:03/32/43</span>
     </div>
-  </div>`
+  </div>`;
 
-  carrContainer.innerHTML += tpl
-})
+	carrContainer.innerHTML += tpl;
+});
 
-btnBuy.addEventListener('click',()=>{
-  localStorage.clear()
-})
-
-
+btnBuy.addEventListener('click', () => {
+	localStorage.clear();
+});
